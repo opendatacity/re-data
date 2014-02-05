efa863eb46917bb3ba79fb5f61fffec966423193
Micho <github@michael-kreil.de>
2014-02-05 09:48:47 +0100

    making scraper async

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,14 +1,21 @@
 #!/usr/bin/env node
 
+var async = require('async');
+
 var db = require('./lib/db');
 var dump = require('./lib/dump');
 
-function finalize(data) {
-	db.update(data);
-	dump.dump(data);
-}
-
-
-require('./rp13/scraper.js').scrape(finalize);
-
-
+async.eachSeries(
+	[
+		require('./rp13/scraper.js')
+	],
+	function (scraper, callback) {
+		scraper.scrape(function (data) {
+			db.update(data);
+			dump.dump(data);
+			callback();
+		});
+	},
+	function () {
+	}
+)
