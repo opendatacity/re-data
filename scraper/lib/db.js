145fa175282b964db75890b7f5449613d83d8590
Micho <github@michael-kreil.de>
2014-02-24 18:16:53 +0100

    adding comments for db.js

--- scraper/lib/db.js
+++ scraper/lib/db.js
@@ -11,75 +11,126 @@ var log = require(path.resolve(__dirname, '../../api/lib/log.js'));
 /* import config */
 var config = require(path.resolve(__dirname, '../../config.js'));
 
+
 exports.update = function (data, callback) {
+
+	// Try to connect to CouchDB ...
+
 	connectCouch(function (db) {
 		if (!db) {
+			log.critical('Can not Update the Database');
 			callback(data);
 			return;
 		}
-		updateCouch(db, data, function (data) {
+
+		// ... and update the database
+
+		updateCouchDB(db, data, function (data) {
 			callback(data);
 		})
 	})
 }
 
-function updateCouch(db, data, callback) {
+function updateCouchDB(db, data, callback) {
+
+	// updateCouchDB deletes the SQL ... no wait ... updates the CouchDB
+
+	// Since we want to check for changes for every entry,
+	// it would be very slow to fetch each single item from the db and
+	// compare it with the new item.
+	// Instead we have a dbCache, that loads every e.g. session of an event
+	// in a single query.
 
 	var dbCache = new (function () {
 		var me = this;
 
-		var couchViews = {
-			'session':'data/sessions',
-			'speaker':'data/speakers',
-			'track':'data/tracks',
-			'location':'data/locations',
-			'day':'data/days',
-			'format':'data/formats',
-			'level':'data/levels',
-			'language':'data/languages'
+		// small lookup to find the corresponding CouchDB for item.type
+		var couchDBViews = {
+			'session':  'data/sessions',
+			'speaker':  'data/speakers',
+			'track':    'data/tracks',
+			'location': 'data/locations',
+			'day':      'data/days',
+			'format':   'data/formats',
+			'level':    'data/levels',
+			'language': 'data/languages'
 		}
 
+		// Here be caches
 		var caches = {};
 
+		// check if you you can find the item in the db
 		me.findInDB = function (item, callback) {
+
+			// Which cache do we want to use? e.g. "rp13-session"
 			var cacheKey = [item.event, item.type].join('-');
+
 			if (caches[cacheKey]) {
+
+				// This cache exists so use the item.id to find the item
 				callback(caches[cacheKey][item.id]);
+
 			} else {
-				var couchView = couchViews[item.type];
-				if (!couchView) {
+
+				// Well the cache does not exists yet.
+				// Lets find the name of the CouchDB view ...
+				var couchDBView = couchDBViews[item.type];
+				if (!couchDBView) {
 					log.critical('Unknown Item Type "'+item.type+'"');
-					callback();
+					callback(false);
 				}
-				log.info('Fetching "'+couchView+'" for "'+item.event+'"');
-				db.view(couchView, {include_docs: true, startkey: [item.event], endkey: [item.event, {}]}, function(err, docs) {
+
+				// ... fetch every entry of this view ...
+				log.info('Fetching "'+couchDBView+'" for "'+item.event+'"');
+				db.view(couchDBView, {include_docs: true, startkey: [item.event], endkey: [item.event, {}]}, function(err, docs) {
+
+					// ... and store these entries in a cache.
 					var cache = {};
 					docs.forEach(function (doc) {
 						cache[doc.id] = doc;
 					});
 					caches[cacheKey] = cache;
-					callback(caches[cacheKey][item.id]);
+
+					// Well, we should return the found item
+					callback(cache[item.id]);
 				})
 			}
 		}
 		return me;
 	})();
 
+	// Now we have a super smooth db lookup with caching ...
+
+	// ... so we could start to check every scraped item ...
 	async.eachSeries(
 		data,
 		function (item, callback) {
+			
+			// ... if it already exsists in the database.
 			dbCache.findInDB(item, function (doc) {
+
 				if (doc) {
+
+					// If it already exists then check if ...
 					if (areEqual(doc, item)) {
+
+						// ... it is still the same so set the last_modified date to the last known changes ...
 						item.last_modified = doc.last_modified;
 						callback();
+
 					} else {
+
+						// ... or if it should be updated in the database.
 						log.info('Updating "'+item.id+'"');
 						db.save(doc._id, item, function () { callback() });
+
 					}
 				} else {
+
+					// If it does not exists in the database, just add the damn thing!
 					log.info('Adding "'+item.id+'"');
 					db.save(item, function () { callback() });
+
 				}
 			});
 		},
@@ -112,30 +163,44 @@ function connectCouch(callback) {
 	});
 };
 
+
 function areEqual(obj1, obj2) {
+
+	// Checks, if two objects are equal (enough)
+
+	// We can ignore these keys
 	var ignoreKeys = {
 		'_id': true,
 		'_rev': true,
 		'last_modified': true
 	};
 
+	// Which keys are used by at least one object
 	var allKeys = {};
 	Object.keys(obj1).forEach(function (key) { allKeys[key] = true });
 	Object.keys(obj2).forEach(function (key) { allKeys[key] = true });
 	
-	var same = true
+	// Now check for differences for each key.
+	var bothAreEqual = true
 	Object.keys(allKeys).forEach(function (key) {
+		// ignore ignoreKeys keys
 		if (ignoreKeys[key]) return;
+
+		// Hashing is done by JSON.stringify
 		var value1 = JSON.stringify(obj1[key]);
 		var value2 = JSON.stringify(obj2[key]);
-		if (value1 != value2) {
-			same = false;
 
+		if (value1 != value2) {
+			bothAreEqual = false;
 			log.info(obj1.id+' changed at "'+key+'" from "'+value1+'" to "'+value2+'"');
 		}
 	});
-	return same;
+
+	return bothAreEqual;
 }
+
+
+
 
 /*
 	
