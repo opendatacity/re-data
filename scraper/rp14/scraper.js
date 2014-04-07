3628c291ce0d6edf5e027553a303cd30650eeaae
Thomas Kollbach <thomas@kollba.ch>
2014-04-07 09:21:02 +0200

    Update tracks with color information and added "other" track Note that there are no session of 're:publica' and 're:campaign' yet, so the color is unknown as of now.

--- scraper/rp14/scraper.js
+++ scraper/rp14/scraper.js
@@ -4,14 +4,15 @@ var fs = require('fs');
 var path = require('path');
 
 var allTracks = {
-	'Business & Innovation':  { id:'business-innovation', label_de:'Business & Innovation',  label_en:'Business & Innovation' },
-	'Science & Technology':   { id:'science-technology',  label_de:'Wissenschaft & Technik', label_en:'Science & Technology'  },
-	'Politics & Society':     { id:'politics-society',    label_de:'Politik & Gesellschaft', label_en:'Politics & Society'    },
-	'Research & Education':   { id:'research-education',  label_de:'Forschung & Bildung',    label_en:'Research & Education'  },
-	'Culture':                { id:'culture',             label_de:'Kultur',                 label_en:'Culture'               },
-	'Media':                  { id:'media',               label_de:'Medien',                 label_en:'Media'                 },
-	're:publica':             { id:'republica',           label_de:'re:publica',             label_en:'re:publica'            },
-	're:campaign':            { id:'recampaign',          label_de:'re:campaign',            label_en:'re:campaign'           }
+	'Business & Innovation':  { id:'business-innovation', label_de:'Business & Innovation',  label_en:'Business & Innovation', color:[194.0, 56.0, 24.0, 0.0] },
+	'Science & Technology':   { id:'science-technology',  label_de:'Wissenschaft & Technik', label_en:'Science & Technology' , color:[0.0, 0.0, 0.0, 0.0] },
+	'Politics & Society':     { id:'politics-society',    label_de:'Politik & Gesellschaft', label_en:'Politics & Society'   , color:[111.0, 79.0, 132.0, 0.0] },
+	'Research & Education':   { id:'research-education',  label_de:'Forschung & Bildung',    label_en:'Research & Education' , color:[0.0, 0.0, 0.0, 1.0] },
+	'Culture':                { id:'culture',             label_de:'Kultur',                 label_en:'Culture'              , color:[193.0, 117.0, 28.0, 1.0] },
+	'Media':                  { id:'media',               label_de:'Medien',                 label_en:'Media'                , color:[78.0, 144.0, 178.0, 1.0] },
+	're:publica':             { id:'republica',           label_de:'re:publica',             label_en:'re:publica'           , color:[0.0, 0.0, 0.0, 1.0] },
+	're:campaign':            { id:'recampaign',          label_de:'re:campaign',            label_en:'re:campaign'          , color:[0.0, 0.0, 0.0, 1.0] },
+	'Other':                  { id:'other',               label_de:'Other',                  label_en:'Other'                , color:[101.0, 156.0, 45.0, 1.0] },
 }
 
 var allFormats = {
