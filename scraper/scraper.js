bd59319b61e1f9beb17d00702f4e67d23613aef1
Thomas Kollbach <thomas@kollba.ch>
2014-05-20 16:20:06 +0200

    makre scraper work with temp API

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -10,8 +10,9 @@ var log = require(path.resolve(__dirname, '../api/lib/log.js'));
 
 async.eachSeries(
 	[
-		{ module:require('./rp13/scraper.js'), db:true },
-		{ module:require('./rp14/scraper.js'), db:true }
+		// { module:require('./rp13/scraper.js'), db:true },
+		// { module:require('./rp14/scraper.js'), db:true }
+		{ module:require('./altconf14/scraper.js'), db:false }
 	],
 	function (item, callback) {
 		item.module.scrape(function (data) {
