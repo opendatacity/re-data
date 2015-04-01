0a2584ed6649f99148a871e0dc07625d233b3339
Thomas Kollbach <thomas@kollba.ch>
2015-04-01 22:00:23 +0200

    add and sort all categories

--- scraper/rp15/scraper.js
+++ scraper/rp15/scraper.js
@@ -18,22 +18,28 @@ var streamURLs = {
 
 var allTracks = {
 	'Business & Innovation':  { id:'business-innovation', label_de:'Business & Innovation',  label_en:'Business & Innovation', color:[194.0, 56.0, 24.0, 1.0] },
-	'Science & Technology':   { id:'science-technology',  label_de:'Wissenschaft & Technik', label_en:'Science & Technology' , color:[168.0, 146.0, 24.0, 1.0] },
-	'Politics & Society':     { id:'politics-society',    label_de:'Politik & Gesellschaft', label_en:'Politics & Society'   , color:[111.0, 79.0, 132.0, 1.0] },
-	'Research & Education':   { id:'research-education',  label_de:'Forschung & Bildung',    label_en:'Research & Education' , color:[0.0, 0.0, 0.0, 1.0] },
+	'City Of The Future':     { id:'city-of-the-future',  label_de:'City Of The Future',     label_en:'City Of The Future', color:[194.0, 56.0, 24.0, 1.0] },	
 	'Culture':                { id:'culture',             label_de:'Kultur',                 label_en:'Culture'              , color:[193.0, 117.0, 28.0, 1.0] },
+	'Fashiontech':            { id:'fashiontech',         label_de:'Fashiontech',            label_en:'Fashiontech'              , color:[193.0, 117.0, 28.0, 1.0] },
+	'GIG':            		  { id:'gig',         		  label_de:'Global Innovation Gathering',            label_en:'Global Innovation Gathering'              , color:[193.0, 117.0, 28.0, 1.0] },	
 	'Media':                  { id:'media',               label_de:'Medien',                 label_en:'Media'                , color:[78.0, 144.0, 178.0, 1.0] },
-	're:publica':             { id:'republica',           label_de:'re:publica',             label_en:'re:publica'           , color:[102.0, 156.0, 44.0, 1.0] },
-	're:campaign':            { id:'recampaign',          label_de:'re:campaign',            label_en:'re:campaign'          , color:[0.0, 0.0, 0.0, 1.0] },
+	'Media Convention':       { id:'media-Convention',    label_de:'Media Convention',       label_en:'Media Convention'                , color:[78.0, 144.0, 178.0, 1.0] },	
+	'Politics & Society':     { id:'politics-society',    label_de:'Politik & Gesellschaft', label_en:'Politics & Society'   , color:[111.0, 79.0, 132.0, 1.0] },
+	're:cord Musicday':       { id:'re-cord-musicday',           label_de:'re:cord Musicday',             label_en:'re:cord Musicday'           , color:[102.0, 156.0, 44.0, 1.0] },
+	're:health':              { id:'re-health',           label_de:'re:health',             label_en:'re:health'           , color:[102.0, 156.0, 44.0, 1.0] },	
+	're:publica':             { id:'re-publica',           label_de:'re:health',             label_en:'re:health'           , color:[102.0, 156.0, 44.0, 1.0] },
+	're:think Mobility':      { id:'re-think-mobility',           label_de:'re:think Mobility',             label_en:'re:think Mobility'           , color:[102.0, 156.0, 44.0, 1.0] },		
+	'Science & Technology':   { id:'science-technology',  label_de:'Wissenschaft & Technik', label_en:'Science & Technology' , color:[168.0, 146.0, 24.0, 1.0] },
+	'Research & Education':   { id:'research-education',  label_de:'Forschung & Bildung',    label_en:'Research & Education' , color:[0.0, 0.0, 0.0, 1.0] },
 	'Other':                  { id:'other',               label_de:'Other',                  label_en:'Other'                , color:[101.0, 156.0, 45.0, 1.0] }
-}
+};
 
 var allFormats = {
 	'Diskussion': { id:'discussion', label_de:'Diskussion', label_en:'Discussion' },
 	'Vortrag':    { id:'talk',       label_de:'Vortrag',    label_en:'Talk'       },
 	'Workshop':   { id:'workshop',   label_de:'Workshop',   label_en:'Workshop'   },
 	'Aktion':     { id:'action',     label_de:'Aktion',     label_en:'Action'     }
-}
+};
 
 var allLevels = {
 	'Beginner':         { id:'beginner',     label_de:'Anfänger',         label_en:'Beginner'     },
@@ -47,9 +53,9 @@ var allLanguages = {
 };
 
 var allDays = {
-	'06.05.2014': { 'id':'rp14-day-1', 'label_de':'6. Mai', 'label_en':'6. May', 'date':'2014-11-23' },
-	'07.05.2014': { 'id':'rp14-day-2', 'label_de':'7. Mai', 'label_en':'7. May', 'date':'2014-11-24' },
-	'08.05.2014': { 'id':'rp14-day-3', 'label_de':'8. Mai', 'label_en':'8. May', 'date':'2014-11-25' },
+	'05.05.2015': { 'id': eventId +'-day-1', 'label_de':'5. Mai', 'label_en':'May 5th', 'date':'2015-05-05' },
+	'06.05.2015': { 'id': eventId +'-day-2', 'label_de':'6. Mai', 'label_en':'May 6th', 'date':'2015-05-06' },
+	'07.05.2015': { 'id': eventId + '-day-3', 'label_de':'7. Mai', 'label_en':'May 7th', 'date':'2015-05-07' },
 };
 
 var allMaps = {
